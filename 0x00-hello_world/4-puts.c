cdbcedc081114eddca26897e4d6f00ffbabe8ca7
Ikechukwu0395 <Ikechukwumacddouglas@gmail.com>
2023-09-07 16:47:12 -0700

    betty can be very strict

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
-*main:This program prints the string
-*Description:Prints"Programming is like building a multilingual puzzle"
-*to the console.
-*return:(0);
-*/
+ * main -This program prints the string
+ * Description:Prints"Programming is like building a multilingual puzzle"
+ * Return:0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle\n");
