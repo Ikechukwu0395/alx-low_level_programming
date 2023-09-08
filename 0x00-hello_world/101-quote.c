8bbedfcff5e1ed3e1ac029242984f72e641f889a
Ikechukwu0395 <Ikechukwumacddouglas@gmail.com>
2023-09-07 18:21:23 -0700

    standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -starting point of te program
+ * Decription:  prints exactly and that piece of art is useful
+ * "Dora Korpar, 2015-10-1"9, followed by a new line, to the standard error.
+ * Return 1
+ */
+
+int main(void)
+("and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n")
+return (1);
