19718c42f2397f1b9b2dd84d1f27e5e09455e34b
Ikechukwu0395 <Ikechukwumacddouglas@gmail.com>
2023-09-07 18:24:22 -0700

    standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,10 @@
  * "Dora Korpar, 2015-10-1"9, followed by a new line, to the standard error.
  * Return 1
  */
-
 int main(void)
-("and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n")
+{
+char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+fprintf(stderr, "%s", s);
 return (1);
+}
