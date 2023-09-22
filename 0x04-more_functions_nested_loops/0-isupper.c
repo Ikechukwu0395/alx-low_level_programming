7d9770539afe3ae1ed227b3f222fca610bb028a4
ikechukwu0395 <ikechukwumacdouglas@gmail.com>
2023-09-22 08:51:45 -0400

    islower

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ * @c: The interget value
+ * Return: 1 if true, 0 if false
+ */
+int _islower(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
