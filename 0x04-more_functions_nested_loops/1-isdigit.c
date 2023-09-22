53e8b12a2259c9a93686498dc40a3632ea750d33
ikechukwu0395 <ikechukwumacdouglas@gmail.com>
2023-09-22 09:31:23 -0400

    modified

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
  * _isdigit - a digit function
- * Description: prints digit
  * @c: user imput
  * Return: 1 or 0
 */
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}
