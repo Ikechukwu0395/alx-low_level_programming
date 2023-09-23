7af563ae41d68a17fb153631b85341ff459a51c3
ikechukwu0395 <ikechukwumacdougls@gmail.com>
2023-09-23 21:01:31 +0100

    a function that prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_numbers - Entry point
+ * Description: prints from 0 till 9
+ * @perimeter: void
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char a[11] = "0123456789$";
+	int i;
+
+	for (i = 0; i <= 12; i++)
+	{
+		putchar(a[i]);
+	}
+	putchar('\n');
+	return (0);
+}
