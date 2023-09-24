a3261b51f765819e3a2faa0b1f9156d9e3608bb2
ikechukwu0395 <ikechukwumacdouglas@gmail.com>
2023-09-24 18:57:36 -0400

    prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - start of my function
+ * @str: character
+ * Description: prints length of tring
+ * Return: void
+*/
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i < 63; i++)
+	{
+		_putchar (str[i]);
+	}
+	_putchar('\n');
+}
