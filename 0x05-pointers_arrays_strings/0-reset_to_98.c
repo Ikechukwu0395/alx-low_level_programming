3379735422af861377ad2d45624d9509fdfed88d
ikechukwu0395 <ikechukwumacdouglas@gmail.com>
2023-09-24 17:25:43 -0400

    My readme on pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - start of my function
+ * @n: integer
+ * Description: pointss to a variable
+ * Return: void
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
