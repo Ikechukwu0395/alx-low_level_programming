846b5bed8a120a031c7d69bb9987d688301ca574
ikechukwu0395 <ikechukwumacdouglas@gmail.com>
2023-09-24 18:28:16 -0400

    returns length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * reset_to_98 - start of my function
+ * @n: integer
+ * Description: pointss to a variable
+ * Return: void
+*/
+
+int _strlen(char *s) {
+    int length = 0;
+    while (s[length] != '\0') {
+        length++;
+
+    }
+     return length;
+}
