e2c489200bff7f4675f20592606c41c6a36885ab
ikechukwu0395 <ikechukwumacdougls@gmail.com>
2023-09-26 23:21:38 +0100

    prints an array of string

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_array - start of my function
+ * @a: 1st integer
+ * @n: 2nd integer
+ * Description: prints an array of integers
+ * Return: void
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d ", a[i]);
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
