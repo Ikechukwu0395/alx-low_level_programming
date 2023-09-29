a1b80bf1842e922f30a7a22a97c4a54ed95a940b
ikechukwu0395 <ikechukwumacdouglas@gmail.com>
2023-09-29 06:20:44 -0400

    copies the string pointed to by src

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcpy - start of my function
+ * @dest: 1st integer
+ * @src: 2nd integer
+ * Description: copies the string pointed to by src
+ * Return: void
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	char *final = dest;
+int i = 0;
+	while (src[i] != '\0')
+{
+	*dest = *src;
+		src++;
+		dest++;
+  }
+dest = '\0';
+	return (final);
+
+}
