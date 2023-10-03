511a22bf59664268135e9359850fbfb3c7fc36df
ikechukwu0395 <ikechukwumacdougls@gmail.com>
2023-10-03 18:36:52 +0100

    nodified

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -13,10 +13,10 @@ void _print_rev_recursion(char *s)
 
 	if (*s == '\0')
 	{
-		putchar('\n');
+		_putchar('\n');
 		return;
 	}
 	else
 	_print_rev_recursion(s + 1);
-		putchar(*s);
+		_putchar(*s);
 }
