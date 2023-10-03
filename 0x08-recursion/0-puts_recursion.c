e992a8e1abeeb96d8a8a9f8ed8c188f811a7291a
ikechukwu0395 <ikechukwumacdougls@gmail.com>
2023-10-03 03:56:04 +0100

    modified

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,6 +10,7 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
+		putchar('\n');
 		return;
 	}
 	else
@@ -17,5 +18,4 @@ void _puts_recursion(char *s)
 		_putchar(*s);
 	_puts_recursion(s + 1);
 	}
-	_putchar('\n');
 }
