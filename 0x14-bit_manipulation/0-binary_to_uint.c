b9d5d9f5f0141b3f7730a347593df2f3431d7d3d
ikechukwu0395 <ikechukwumacdougls@gmail.com>
2023-11-23 23:21:09 +0100

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,28 +1,28 @@
 #include "main.h"
 
 /**
-* binary_to_uint - Entry point
-* Description: Converts a binary number to an unsigned int
-* @binaryString: Pointer to a string of 0 and 1 chars
-* Return: The converted number
-*/
+ * binary_to_uint - Entry point
+ * Description: Converts a binary number to an unsigned int
+ * @b: Pointer to a string of 0 and 1 chars
+ *
+ * Return: The converted number, or 0 if b is NULL or there's a non 0 or 1 char
+ */
 
-unsigned int binary_to_uint(const char *binaryString)
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int digit = 0;
-
 	unsigned int i = 0;
 
-	/* Check if binaryString is NULL */
-	if (!binaryString)
+	/* Check if b is NULL */
+	if (!b)
 	{
 		return (0);
 	}
 	/* Loop: gets the length of the binary string */
-	while (binaryString[i] != '\0')
+	while (b[i] != '\0')
 	{
 		/* Checks if char is not 0 or 1 */
-		if (binaryString[i] != '0' && binaryString[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 		{
 			return (0);
 		}
@@ -30,11 +30,11 @@ unsigned int binary_to_uint(const char *binaryString)
 	}
 	/* Get the decimal value of the binary string */
 	i = 0;
-	while (binaryString[i] != '\0')
+	while (b[i] != '\0')
 	{
 		/* Left shift the digit by 1 bit or space */
 		digit <<= 1;
-		if (binaryString[i] == '1')
+		if (b[i] == '1')
 		{
 			digit += 1;
 		}
@@ -42,4 +42,3 @@ unsigned int binary_to_uint(const char *binaryString)
 	}
 	return (digit);
 }
-
